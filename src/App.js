df5d1de81603a259bd08da9bf87333a45637f4e8
youngme92 <youngme992@gmail.com>
2020-06-22 18:32:53 +0900

    Extra attributes and Mixins

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, {Fragment} from 'react';
-import styled, { createGlobalStyle, css, Keyframes, keyframes } from "styled-components";
+import styled, { createGlobalStyle, css } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body{
     padding: 0;
     margin: 0;
   }
+`;
+const awsomeCard = css `
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+`;
+
+const Input = styled
+    .input
+    .attrs({required: true, placeholder: "hello"})`
+      border: none;
+      ${awsomeCard}
 `;
 
 const Container = styled.div`
@@ -14,50 +27,13 @@ width: 100%;
 background-color: pink;
 `;
 
-const Button = styled.button`
-border-radius: 50px;
-padding: 5px;
-min-width: 120px;
-color: white;
-font-weight: 600;
--webkit-appearance: none;
-cursor: pointer;
-&:active,
-&:focus: {
- outline: none;
-}
-background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71" )};
-
-${props => {
-  if(props.danger){
-    return css`animation: ${rotation} ${props.rotationTime}s linear infinite`;
-  }
-}}
-`;
-
-// Button의 style을 복제하고 확장한다.
-const Anchor = styled(Button.withComponent("a"))`
-  text-decoration: none;
-`;  
-
-const rotation = keyframes`
-  from{
-    transform: rotate(0deg);
-  }
-  to{
-    transform: rotate(360deg);
-  }
-`;
-
 function App() {
     return (
         <div className="App">
           <Fragment>
             <GlobalStyle />
                 <Container>
-                    <Button>Hello</Button>
-                    <Button danger rotationTime={2}>Hello</Button>
-                    <Anchor href="http://google.com">Go to google</Anchor>
+                  <Input />
                 </Container>
           </Fragment>  
         </div>
